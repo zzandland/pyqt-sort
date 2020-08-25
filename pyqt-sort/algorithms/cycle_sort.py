ead674254d9666c28a2e24d16066d994cf829b26
Si Yong Kim <zzandland@gmail.com>
2020-08-25 16:50:35 -0400

    regular cycle sort

--- pyqt-sort/algorithms/cycle_sort.py
+++ pyqt-sort/algorithms/cycle_sort.py
@@ -5,6 +5,42 @@ class CycleSort(Iterator):
 
     def __init__(self, data: List[int]):
         super().__init__(data)
+        self.i = self.j = 0
+        self.k = self.i + 1
+        self.found = False
+        self.end = False
+        self.seen = set()
+
+    def getPointers(self) -> Tuple[int, int]:
+        return (self.j, self.k)
+
+    def hasNext(self) -> bool:
+        return not self.end
+
+    def next(self) -> List[int]:
+        if self.found:
+            self.seen.add(self.j)
+            if self.j != self.i:
+                self.data[self.i], self.data[self.j] = self.data[self.j], self.data[self.i]
+            else:
+                while self.i in self.seen:
+                    self.i += 1
+                    if self.i == len(self.data) - 1:
+                        self.end = True
+                        break
+            self.found = False
+            self.j = self.i
+            self.k = self.i + 1
+        else:
+            if self.data[self.i] > self.data[self.k]: self.j += 1
+            self.k += 1
+            if self.k == len(self.data): self.found = True
+        return self.data
+
+class OptimizedCycleSort(Iterator):
+
+    def __init__(self, data: List[int]):
+        super().__init__(data)
         self.i = 0
         self.end = False
 
@@ -18,6 +54,7 @@ class CycleSort(Iterator):
         val = self.data[self.i]
         if val != self.i + 1:
             self.data[self.i], self.data[val-1] = self.data[val-1], self.data[self.i]
-        else: self.i += 1
+        else:
+            self.i += 1
         if self.i == len(self.data): self.end = True
         return self.data
